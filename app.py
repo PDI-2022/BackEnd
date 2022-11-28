aa7473638d919785a8b0d9488c900e704801a17a
Matheus Rocha <monteiromatheus.rocha@gmail.com>
2022-11-28 17:59:35 -0300

    adicionando webview

--- app.py
+++ app.py
@@ -3,6 +3,9 @@ from api.routes.model import model_bp
 from api.routes.view import view_bp
 from flask_cors import CORS
 from config import app
+import threading
+import sys
+import webview
 
 CORS(app,origins=["*"])
 
@@ -10,5 +13,21 @@ app.register_blueprint(process_bp)
 app.register_blueprint(model_bp)
 app.register_blueprint(view_bp)
 
-if __name__ == 'main':
-  app.run(debug=True)
+
+def start_server():
+    app.run(host='0.0.0.0', port=5000)
+
+
+if __name__ == '__main__':
+    t = threading.Thread(target=start_server)
+    t.daemon = True
+    t.start()
+
+    webview.create_window('TetraSeed - Classificação de Sementes de Feijão', url='http://localhost:5000/',width=800, height=600, \
+                      resizable=True, fullscreen=False, \
+                      min_size=(1280, 768), hidden=False, frameless=False, \
+                      minimized=False, on_top=False, confirm_close=True, \
+                      background_color='#FFF', text_select=False)
+
+    webview.start()
+    sys.exit()

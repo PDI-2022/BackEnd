7b388e44a71e05faff0ceada89eb7b04f59494ac
Matheus Rocha <monteiromatheus.rocha@gmail.com>
2022-11-29 19:27:31 -0300

    tentando usar o python 3.6 para compatibilizar com o windows 7

--- build.bat
+++ build.bat
@@ -1,3 +1,5 @@
-@REM Gerando os arquivos para deploy em /dist
-pyinstaller -n "tetraseed" --icon ".\static\Assets\Logo\logo.ico" --onefile --clean --add-data "db;db" --add-data "api;api" --add-data "static;static" --add-data "migrations;migrations" --add-data "models;models" --add-data "views;views" --hidden-import "flask_api.parsers" --hidden-import "flask_api.renderers" app.py
-
+@REM python -m pip install --upgrade pip
+@REM pip install -r requirements.txt
+@REM subsituir o hook-shapely no env
+pyinstaller -n "tetraseed" --icon ".\static\Assets\Logo\logo.ico" --clean --add-data "db;db" --add-data "instance;instance" --add-data "api;api" --add-data "static;static" --add-data "migrations;migrations" --add-data "models;models" --add-data "views;views" --hidden-import "flask_api.parsers" --hidden-import "flask_api.renderers" --hidden-import "Shapely.libs" app.py
+@REM --onefile
